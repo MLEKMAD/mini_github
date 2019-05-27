ea52b2cc96c03bf3a56a5c325e70c22a58eb51cf
LEKMAD Mohamed <mohamed.lekmad@gmail.com>
2019-05-27 04:37:09 +0000

    changing html

---
+++ actions/actions.js
@@ -0,0 +1,15 @@
+function action(id,state){
+switch(state){
+case 'approved':
+    document.getElementById(id).innerHTML ="approved";break;
+case "rejected":
+    document.getElementById(id).innerHTML ="rejected";break;
+case 'waiting':
+document.getElementById(id).innerHTML ="waiting";break;
+}
+
+ 
+   
+
+
+}
